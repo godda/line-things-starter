79bf2527c7f01712e0aa9c9e3acb096ee39f04b6
GoDDa <godda.binahead@gmail.com>
2019-01-03 10:12:32 +0700

    Add files via upload

--- liff-app/liff.js
+++ liff-app/liff.js
@@ -1,5 +1,5 @@
 // User service UUID: Change this to your generated service UUID
-const USER_SERVICE_UUID         = 'b481d4b1-5629-4d14-89be-db9f07663ef7'; // LED, Button
+const USER_SERVICE_UUID         = '34f0c9c1-8409-4d1a-85b1-7c3ca0012422'; // LED, Button
 // User service characteristics
 const LED_CHARACTERISTIC_UUID   = 'E9062E71-9E62-4BC6-B0D3-35CDCD9B027B';
 const BTN_CHARACTERISTIC_UUID   = '62FBD229-6EDD-4D1A-B554-5C4E1BB29169';
